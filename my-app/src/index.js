24f132aae4235913b4a5691c87fea0bf0ca6934c
maksim pogribnyak <maksim.pogribnyak@onedome.com>
2018-03-20 20:03:11 +0200

    react-redux

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,133 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import API from './api';
+import {createStore} from 'redux';
+import {connect, Provider} from 'react-redux';
 
-class App extends React.Component {
-    constructor(props) {
-        super(props);
+const intialState = {
+    value: 0
+}
 
-        this.state = {
-            posts: []
-        };
-    }
-
-    componentDidMount() {
-        API.getPost()
-            .then(data => this.setState({posts: data}));
-    }
-
-    removePost = id => () => {
-        API.removePost(id)
-            .then(isSuccess => {
-                if (isSuccess) {
-                    const {posts} = this.state,
-                        removeElementIndex = posts.findIndex(p => p.id === id);
-
-                    posts.splice(removeElementIndex, 1);
-
-                    this.setState({posts});
-                }
-            })
-    }
-
-    handleSubmit = e => {
-        e.preventDefault();
-
-        console.log(this.textareaHTML.value);
-        console.log(this.inputHTML.value);
-    }
-
-    renderForm() {
-        return (
-            <div>
-                <hr/>
-                <form onSubmit={this.handleSubmit}>
-                    <h2>ADD POST</h2>
-                    <p>
-                        <label>
-                            Header:
-                            <input
-                                ref={instance => this.inputHTML = instance}
-                                type="text"/>
-                        </label>
-                    </p>
-                    <p>
-                        <label>
-                            Body
-                            <textarea
-                                ref={instance => this.textareaHTML = instance}
-                                cols='30'
-                                rows='5'></textarea>
-                        </label>
-                    </p>
-                    <button type='submit'>
-                        ADD POST
-                    </button>
-                </form>
-                <hr/>
-            </div>
-        );
-    }
-
-    renderPosts() {
-        const {posts} = this.state;
-
-        if (posts.length > 0) {
-            return (
-                <ul>
-                    {posts.map(post =>
-                        <li key={post.id}>
-                            <h3>{post.title}</h3>
-                            <p>{post.body}</p>
-                            <button onClick={this.removePost(post.id)}>
-                                remove post
-                            </button>
-                        </li>)}
-                </ul>
-            );
+const rootReducer = (state = intialState, action) => {
+    switch(action.type) {
+        case 'INCREASE': return {
+            ...state,
+            value: state.value + 1
         }
-
-        return <div>Loading...</div>;
-    }
-
-    render() {
-        return (
-            <div>
-                {this.renderForm()}
-                <h1>POSTS</h1>
-                {this.renderPosts()}
-            </div>
-        );
+        case 'DECREASE': return {
+            ...state,
+            value: state.value - 1
+        }
+        default:
+            return state;
     }
 }
 
+let store = createStore(rootReducer);
+
+class App extends React.Component {
+    render() {
+        console.log(this.props);
+
+        return (
+            <div/>
+        )
+    }
+}
+
+const SmartApp = connect(state => state)(App);
+
 ReactDOM.render(
-    <App/>,
+    <Provider store={store}>
+        <SmartApp/>
+    </Provider>,
     document.getElementById('root')
 );
-
-
-
-
-
-
-
-
-
-
-
-
-
-// fetch('https://jsonplaceholder.typicode.com/posts', {
-//     method: 'POST',
-//     body: JSON.stringify({
-//         title: 'Hello',
-//         body: 'World',
-//         userId: 1123123
-//     }),
-//     headers: {
-//         "Content-type": "application/json; charset=UTF-8"
-//     }
-// }).then(r => r.json()).then(r => console.log(r));
-
-// fetch('https://jsonplaceholder.typicode.com/posts/?userId=1123123');
